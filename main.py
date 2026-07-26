d9dcbb8fff94d375ed7e8522fd6b806ad98b3901
Carlos-Espinoza77 <ecarlosi1977@gmail.com>
2026-07-25 22:39:40 -0500

    adding flask

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("hola")   
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")# es un decorador que indica que la función hello_world() se ejecutará cuando se acceda a la ruta raíz del servidor web.
+def hello_world():
+    return render_template("index.html")# devuelve el contenido de la plantilla HTML "index.html" como respuesta a la solicitud HTTP.
